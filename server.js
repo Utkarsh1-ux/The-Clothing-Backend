6bcaf0273c7657827e3fccd610f0114fa67cf3d6
Utkarsh1-ux <utkarshramu53@gmail.com>
2025-12-26 17:51:14 +0530

    created model and connected databases

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import { configDotenv } from 'dotenv'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js';
+
+configDotenv();
+// App config
+const app = express()
+const port = process.env.PORT || 3000
+connectDB();
+connectCloudinary();
+
+// middlewares
+
+app.use(express.json())
+app.use(cors())
+
+// Api endpoints
+
+app.get('/' , (req,res)=>{
+    res.send("Api working")
+})
+
+app.listen(port , ()=> console.log("server started on port : " + port))
