e997a4837e0002e553a21c86ebc29a0b09b41fe2
Utkarsh1-ux <utkarshramu53@gmail.com>
2026-01-19 17:12:16 +0530

    contollers new cart

---
+++ controllers/cartController.js
@@ -0,0 +1,18 @@
+ 
+
+ //add product to user Cart
+ const addToCart = async (req,res) => {
+    
+ }
+
+ //update user Cart
+ const updateCart = async (req,res) => {
+    
+ }
+
+ //get user Cart data
+ const getUserCart = async (req,res) => {
+    
+ }
+
+ export{addToCart,updateCart,getUserCart }
---
+++ routes/cartRoute.js
@@ -0,0 +1,8 @@
+import express from "express"
+import { addToCart,getUserCart,updateCart } from "../controllers/cartController.js"
+
+const cartRouter  = express.Router()
+
+cartRouter.post('/get',getUserCart)
+cartRouter.post('/add',addToCart)
+cartRouter.post('/update',updateCart)
