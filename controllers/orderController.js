d44211472ea0cbb925a9d05c71e4d10442d2df29
Utkarsh1-ux <utkarshramu53@gmail.com>
2026-03-20 20:53:21 +0530

    added functionality and controllers

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -45,12 +45,32 @@ const placeOrderRazorpay = async (req , res) =>{
 
 //All orders data for admin pannel 
 const allOrders = async (req , res) =>{
-    
+    try {
+       const orders = await orderModel.find({})
+       res.json({success : true , orders})
+
+    } catch (error) {
+      
+      console.log(error);
+    res.json({success: false , message: error.message})
+
+    }
 }
 
 //User orders data for Frontend
 const userOrders = async (req , res) =>{
+  try {
+     
+     const {userId} = req.body
 
+     const orders = await orderModel.find({userId})
+     res.json({success : true , orders})
+
+  } catch (error) {
+     console.log(error);
+    res.json({success: false , message: error.message})
+
+  }
 }
 
 //Update order status from Admin panel
